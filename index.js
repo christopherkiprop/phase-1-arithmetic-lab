d2dbe3bd9dfbb36aa4ecf1b0da4e12354a02aad8
christopherkiprop <choppachepa@gmail.com>
2024-07-12 22:03:01 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+// Task 1: multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Task 2: random
+let random = Math.ceil(Math.random() * 100);
+
+// Task 3: mod
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+// Task 4: max
+let max = Math.max(1, 2, 5, 20, 19);
+
+// Don't change anything below this line
+module.exports = {
+  multiply,
+  random,
+  mod,
+  max
+};
